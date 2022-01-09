5593023804126dae3b38d5ba675fc12c151248ad
sogolhaghani <haghani.sogol@gmail.com>
2022-01-09 22:19:17 +0330

    continue on clear data

--- src/_triple_extraction.py
+++ src/_triple_extraction.py
@@ -1,5 +1,6 @@
 import spacy
 from spacy import displacy
+import re
 
 
 def _extract_head(doc):
@@ -15,15 +16,15 @@ def _extract_triple(_read_directory, _csv_name, _write_directory, _file_name):
     nlp.max_length = 3030000 
     with open(_read_directory + _csv_name, 'r') as inp:
         for line in inp:
-            doc = nlp(line.strip())
-            _heads = _extract_head(doc)
-            _has_verb = False
-            _has_obj = False
-            for token in doc:
-                if token.pos_ == 'VERB':
-                    _has_verb = True
-                if token.dep_ == 'pobj' or token.dep_ == 'dobj':
-                    _has_obj = True
+            # doc = nlp(line.strip())
+            # _heads = _extract_head(doc)
+            # _has_verb = False
+            # _has_obj = False
+            # for token in doc:
+            #     if token.pos_ == 'VERB':
+            #         _has_verb = True
+            #     if token.dep_ == 'pobj' or token.dep_ == 'dobj':
+            #         _has_obj = True
 
             # if len(_heads) == 0:
             #     print(line)
@@ -33,8 +34,20 @@ def _extract_triple(_read_directory, _csv_name, _write_directory, _file_name):
 
 
             # TODO remove these patern from raw text
-            if len(_heads) == 0 and len(doc) > 2 and _has_verb == False and _has_obj == False:
+            # if len(_heads) == 0 and len(doc) > 2 and _has_verb == False and _has_obj == False:
+            #     print(line)
+
+            # if len(_heads) == 0 and len(doc) > 2 and _has_verb == True and _has_obj == False:
+            #     print(line)  
+            # TODO :// replace cite
+            # pattern = r'\(([^"\)]*|\bAnonymous\b|"[^"\)]*")(, )([\d]+|n\.d\.|[\d]+[\w])\)'
+            pattern = r'^(?:[a-zA-Z]\.|[0-9]\.)+$'
+            results = re.finditer(pattern, line.strip())
+            for match in results:
+                print(match)
                 print(line)
+                break
+          
 
 
 
